b5593be454f657e8bb76d62e32265db2f6d6a8ef
David Quagebeur <david.quagebeur@atos.net>
2015-04-13 12:23:47 +0200

    correct bug when forwarding request on bad domain + AAAA from etcd

--- etcd.go
+++ etcd.go
@@ -1,106 +1,106 @@
 package main
 
 import (
-  "github.com/coreos/go-etcd/etcd"
-  "github.com/miekg/dns"
-  "log"
-  "net"
-  "path"
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/miekg/dns"
+	"log"
+	"net"
+	"path"
 )
 
 type Response interface {
-  Value() string
-  Node() *etcd.Node
+	Value() string
+	Node() *etcd.Node
 }
 
 type Client interface {
-  Get(path string) (Response, error)
-  WatchForChanges()
-  GetAll(path string) []*etcd.Node
+	Get(path string) (Response, error)
+	WatchForChanges()
+	GetAll(path string) []*etcd.Node
 }
 
 type EtcdClient struct {
-  InstanceUrl string
-  Client      *etcd.Client
+	InstanceUrl string
+	Client      *etcd.Client
 }
 
 type EtcdResponse struct {
-  Response *etcd.Response
+	Response *etcd.Response
 }
 
 func NewEtcdClient(instanceUrl string) Client {
-  return &EtcdClient{
-    InstanceUrl: instanceUrl,
-    Client: etcd.NewClient([]string{instanceUrl}),
-  }
+	return &EtcdClient{
+		InstanceUrl: instanceUrl,
+		Client:      etcd.NewClient([]string{instanceUrl}),
+	}
 }
 
 func (r EtcdResponse) Node() *etcd.Node {
-  return r.Response.Node;
+	return r.Response.Node
 }
 
 func (r EtcdResponse) Value() string {
-  return r.Response.Node.Value;
+	return r.Response.Node.Value
 }
 
 func validate(node *etcd.Node) (bool, string) {
-  recordType := path.Base(node.Key)
-  switch recordType {
-    case "A":
-      return net.ParseIP(node.Value) != nil, "Invalid ip"
-    case "CNAME", "PTR":
-      return dns.IsFqdn(node.Value), "Domain name not fully-qualified"
-    case "SOA":
-      return true, ""
-    default:
-      return false, "Record type not supported"
-  }
+	recordType := path.Base(node.Key)
+	switch recordType {
+	case "A", "AAAA":
+		return net.ParseIP(node.Value) != nil, "Invalid ip"
+	case "CNAME", "PTR":
+		return dns.IsFqdn(node.Value), "Domain name not fully-qualified"
+	case "SOA":
+		return true, ""
+	default:
+		return false, "Record type not supported"
+	}
 }
 
 func (c EtcdClient) GetAll(key string) []*etcd.Node {
-  response, _ := c.Client.Get(key, false, true)
+	response, _ := c.Client.Get(key, false, true)
 
-  nodes := []*etcd.Node{}
+	nodes := []*etcd.Node{}
 
-  for _, node := range response.Node.Nodes {
-    nodes = append(nodes, node)
-  }
+	for _, node := range response.Node.Nodes {
+		nodes = append(nodes, node)
+	}
 
-  return nodes
+	return nodes
 }
 
 func (c EtcdClient) WatchForChanges() {
-  log.Printf("Setting up watch to validate entries")
-  channel := make(chan *etcd.Response)
+	log.Printf("Setting up watch to validate entries")
+	channel := make(chan *etcd.Response)
 
-  go func() {
-    c.Client.Watch("/helix", 0, true, channel, nil)
-  }()
+	go func() {
+		c.Client.Watch("/helix", 0, true, channel, nil)
+	}()
 
-  defer c.catchEtcdPanic()
+	defer c.catchEtcdPanic()
 
-  for {
-    select {
-      case resp := <-channel:
-        if valid, msg := validate(resp.Node); !valid {
-          log.Printf("ERROR: %s (%s => %s)", msg, resp.Node.Key, resp.Node.Value)
-        }
-    }
-  }
+	for {
+		select {
+		case resp := <-channel:
+			if valid, msg := validate(resp.Node); !valid {
+				log.Printf("ERROR: %s (%s => %s)", msg, resp.Node.Key, resp.Node.Value)
+			}
+		}
+	}
 }
 
 func (c EtcdClient) catchEtcdPanic() {
-  if r := recover(); r != nil {
-    log.Fatalf("Panic in setting up watch on /helix. Is etcd running at %s ?", c.InstanceUrl)
-  }
+	if r := recover(); r != nil {
+		log.Fatalf("Panic in setting up watch on /helix. Is etcd running at %s ?", c.InstanceUrl)
+	}
 }
 
 func (c EtcdClient) Get(path string) (Response, error) {
-  resp, err := c.Client.Get(path, false, false)
+	resp, err := c.Client.Get(path, false, false)
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  return &EtcdResponse{ Response: resp }, nil
+	return &EtcdResponse{Response: resp}, nil
 }
--- server.go
+++ server.go
@@ -1,211 +1,216 @@
 package main
 
 import (
-  "encoding/json"
-  "github.com/miekg/dns"
-  "log"
-  "strconv"
-  "strings"
-  "time"
-  "github.com/coreos/go-etcd/etcd"
+	"encoding/json"
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/miekg/dns"
+	"log"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type HelixServer struct {
-  Port      int
-  Client    Client
-  DNSClient DNSClient
+	Port      int
+	Client    Client
+	DNSClient DNSClient
 }
 
 func ForwardingServer(port int, etcdurl, dnsServerUrl string) HelixServer {
-  client := NewEtcdClient(etcdurl)
-  dnsClient := ForwardingDNSClient{ Address: dnsServerUrl }
+	client := NewEtcdClient(etcdurl)
+	dnsClient := ForwardingDNSClient{Address: dnsServerUrl}
 
-  return HelixServer{ Port: port, Client: client, DNSClient: dnsClient }
+	return HelixServer{Port: port, Client: client, DNSClient: dnsClient}
 }
 
 func Server(port int, etcdurl string) HelixServer {
-  client := NewEtcdClient(etcdurl)
+	client := NewEtcdClient(etcdurl)
 
-  return HelixServer{ Port: port, Client: client }
+	return HelixServer{Port: port, Client: client}
 }
 
 func (s HelixServer) Start() {
-  server := &dns.Server{
-    Addr:         ":"+strconv.Itoa(s.Port),
-    Net:          "udp",
-    Handler:      dns.HandlerFunc(s.Handler),
-    ReadTimeout:  10 * time.Second,
-    WriteTimeout: 10 * time.Second,
-  }
+	server := &dns.Server{
+		Addr:         ":" + strconv.Itoa(s.Port),
+		Net:          "udp",
+		Handler:      dns.HandlerFunc(s.Handler),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 
-  zoneTransferServer := &dns.Server{
-    Addr:         ":"+strconv.Itoa(s.Port),
-    Net:          "tcp",
-    Handler:      dns.HandlerFunc(s.zoneTransferHandler),
-    ReadTimeout:  10 * time.Second,
-    WriteTimeout: 10 * time.Second,
-  }
+	zoneTransferServer := &dns.Server{
+		Addr:         ":" + strconv.Itoa(s.Port),
+		Net:          "tcp",
+		Handler:      dns.HandlerFunc(s.zoneTransferHandler),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 
-  go s.Client.WatchForChanges()
+	go s.Client.WatchForChanges()
 
-  log.Print("Starting servers...")
+	log.Print("Starting servers...")
 
-  go server.ListenAndServe()
-  go zoneTransferServer.ListenAndServe()
+	go server.ListenAndServe()
+	go zoneTransferServer.ListenAndServe()
 }
 
 func domainToEtcdKey(domain, record string) string {
-  addr := dns.SplitDomainName(domain)
-  path := []string{"helix"}
+	addr := dns.SplitDomainName(domain)
+	path := []string{"helix"}
 
-  for i := len(addr) - 1; i >= 0; i-- {
-    path = append(path, addr[i])
-  }
+	for i := len(addr) - 1; i >= 0; i-- {
+		path = append(path, addr[i])
+	}
 
-  if record != "" {
-    path = append(path, record)
-  }
+	if record != "" {
+		path = append(path, record)
+	}
 
-  return strings.Join(path, "/")
+	return strings.Join(path, "/")
 }
 
 func (s HelixServer) getResponse(q dns.Question) (Response, error) {
-  value := dns.TypeToString[q.Qtype]
-  return s.Client.Get(domainToEtcdKey(q.Name, value))
+	value := dns.TypeToString[q.Qtype]
+	return s.Client.Get(domainToEtcdKey(q.Name, value))
 }
 
 func addNode(records chan dns.RR, node *etcd.Node) {
-  if node.Dir {
-    for i := range node.Nodes {
-      addNode(records, node.Nodes[i])
-    }
-  } else {
-    value := etcdNodeToDnsRecord(node)
-    for i := range value {
-      records <- value[i]
-    }
-  }
+	if node.Dir {
+		for i := range node.Nodes {
+			addNode(records, node.Nodes[i])
+		}
+	} else {
+		value := etcdNodeToDnsRecord(node)
+		for i := range value {
+			records <- value[i]
+		}
+	}
 }
 
 func (s HelixServer) recordsForDomain(domain string) chan dns.RR {
 
-  nodes := s.Client.GetAll(domainToEtcdKey(domain, ""))
+	nodes := s.Client.GetAll(domainToEtcdKey(domain, ""))
 
-  leafNodes := make(chan dns.RR, 0)
+	leafNodes := make(chan dns.RR, 0)
 
-  go func() {
-    for i := range nodes {
-      addNode(leafNodes, nodes[i])
-    }
-    close(leafNodes)
-  }()
+	go func() {
+		for i := range nodes {
+			addNode(leafNodes, nodes[i])
+		}
+		close(leafNodes)
+	}()
 
-  return leafNodes
+	return leafNodes
 }
 
 func (s HelixServer) zoneTransferHandler(w dns.ResponseWriter, req *dns.Msg) {
 
-  value := req.Question[0].Qtype
+	value := req.Question[0].Qtype
 
-  switch value {
-  case dns.TypeAXFR, dns.TypeIXFR:
-    domain := req.Question[0].Name
+	switch value {
+	case dns.TypeAXFR, dns.TypeIXFR:
+		domain := req.Question[0].Name
 
-    resp, err := s.getResponse(dns.Question{Qtype:dns.TypeSOA,Name:domain })
+		resp, err := s.getResponse(dns.Question{Qtype: dns.TypeSOA, Name: domain})
 
-    if err != nil {
-      log.Printf("Could not find SOA for %s", domain)
-      return
-    }
+		if err != nil {
+			log.Printf("Could not find SOA for %s", domain)
+			return
+		}
 
-    c := make(chan *dns.Envelope)
-    transfer := new(dns.Transfer)
+		c := make(chan *dns.Envelope)
+		transfer := new(dns.Transfer)
 
-    defer close(c)
+		defer close(c)
 
-    err = transfer.Out(w, req, c)
+		err = transfer.Out(w, req, c)
 
-    if err != nil {
-      log.Printf("Could not begin zone transfer.")
-      return
-    }
+		if err != nil {
+			log.Printf("Could not begin zone transfer.")
+			return
+		}
 
-    var soa *dns.SOA
+		var soa *dns.SOA
 
-    err = json.Unmarshal([]byte(resp.Value()), &soa)
+		err = json.Unmarshal([]byte(resp.Value()), &soa)
 
-    if err != nil {
-      log.Printf("Failed to parse SOA record: %s", resp.Value())
-      return
-    }
+		if err != nil {
+			log.Printf("Failed to parse SOA record: %s", resp.Value())
+			return
+		}
 
-    header := dns.RR_Header{Name: domain, Rrtype: dns.TypeSOA, Class: dns.ClassINET, Ttl: 5}
-    soa.Hdr = header
+		header := dns.RR_Header{Name: domain, Rrtype: dns.TypeSOA, Class: dns.ClassINET, Ttl: 5}
+		soa.Hdr = header
 
-    records := []dns.RR{soa}
+		records := []dns.RR{soa}
 
-    channel := s.recordsForDomain(domain)
-    for record := range channel {
-      records = append(records, record)
-    }
+		channel := s.recordsForDomain(domain)
+		for record := range channel {
+			records = append(records, record)
+		}
 
-    records = append(records, soa)
+		records = append(records, soa)
 
-    c <- &dns.Envelope{RR: records}
+		c <- &dns.Envelope{RR: records}
 
-    w.Hijack()
-    return
-    default:
-      log.Printf("Was not a zone transfer request.")
-    }
+		w.Hijack()
+		return
+	default:
+		log.Printf("Was not a zone transfer request.")
+	}
 
 }
 
 func (s HelixServer) Handler(w dns.ResponseWriter, req *dns.Msg) {
-  m := new(dns.Msg)
-  m.SetReply(req)
+	m := new(dns.Msg)
+	m.SetReply(req)
 
-  qType  := req.Question[0].Qtype
-  qClass := req.Question[0].Qclass
+	qType := req.Question[0].Qtype
+	qClass := req.Question[0].Qclass
 
-  header := dns.RR_Header{Name: m.Question[0].Name, Rrtype: qType, Class: qClass, Ttl: 5}
-  resp, err := s.getResponse(req.Question[0])
+	header := dns.RR_Header{Name: m.Question[0].Name, Rrtype: qType, Class: qClass, Ttl: 5}
+	resp, err := s.getResponse(req.Question[0])
 
-  if err != nil {
-    if s.DNSClient != nil {
-      log.Printf("Could not get record for %s, forwarding to %s", req.Question[0].Name, s.DNSClient.GetAddress())
-      in, _ := s.DNSClient.Lookup(req)
-      w.WriteMsg(in)
-    } else {
-      log.Printf("Could not get record for %s", req.Question[0].Name)
-      w.WriteMsg(m)
-    }
-    return
-  }
+	if err != nil {
+		if s.DNSClient != nil {
+			log.Printf("Could not get record for %s, forwarding to %s", req.Question[0].Name, s.DNSClient.GetAddress())
+			in, _ := s.DNSClient.Lookup(req)
+			if err != nil {
+				log.Printf("Could not get record from %s : %s", s.DNSClient.GetAddress(), err.Error)
+				w.WriteMsg(m)
+			} else {
+				w.WriteMsg(in)
+			}
+		} else {
+			log.Printf("Could not get record for %s", req.Question[0].Name)
+			w.WriteMsg(m)
+		}
+		return
+	}
 
-  switch qType {
-    case dns.TypeA, dns.TypeAAAA, dns.TypeCNAME, dns.TypePTR:
-      m.Answer = etcdNodeToDnsRecord(resp.Node())
-    case dns.TypeSRV:
-      var records []SrvRecord
-      err := json.Unmarshal([]byte(resp.Value()), &records)
-      if err != nil {
-        log.Printf("Could not unmarshal SRV record from etcd: %s", resp.Value())
-      } else {
-        m.Answer = make([]dns.RR, len(records))
-        for i := range records {
-          m.Answer[i] = &dns.SRV {
-            Hdr:      header,
-            Priority: records[i].Priority,
-            Weight:   records[i].Weight,
-            Port:     records[i].Port,
-            Target:   records[i].Target,
-          }
-        }
-      }
-    default:
-      log.Printf("Unrecognised record type: %d",qType)
-  }
+	switch qType {
+	case dns.TypeA, dns.TypeAAAA, dns.TypeCNAME, dns.TypePTR:
+		m.Answer = etcdNodeToDnsRecord(resp.Node())
+	case dns.TypeSRV:
+		var records []SrvRecord
+		err := json.Unmarshal([]byte(resp.Value()), &records)
+		if err != nil {
+			log.Printf("Could not unmarshal SRV record from etcd: %s", resp.Value())
+		} else {
+			m.Answer = make([]dns.RR, len(records))
+			for i := range records {
+				m.Answer[i] = &dns.SRV{
+					Hdr:      header,
+					Priority: records[i].Priority,
+					Weight:   records[i].Weight,
+					Port:     records[i].Port,
+					Target:   records[i].Target,
+				}
+			}
+		}
+	default:
+		log.Printf("Unrecognised record type: %d", qType)
+	}
 
-  w.WriteMsg(m)
+	w.WriteMsg(m)
 }
